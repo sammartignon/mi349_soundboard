96dc590c1a5ced7700813b492e66e0f7c88af0dc
sammartignon <martign2@msu.edu>
2022-11-16 10:43:24 -0500

    Testing commit

--- soundboard.js
+++ soundboard.js
@@ -1,5 +1,11 @@
 var buttonElement = document.getElementById('button1')
 
 buttonElement.addEventListener('mouseenter', function () {
-    console.log('I was Hovered on!!')
+    console.log('I was Hovered on!!'),
+    boo.wav.play()
+  })
+
+  buttonElement.addEventListener('click', function () {
+    console.log('I was Clicked'),
+    applause2_x.wav.play()
   })
